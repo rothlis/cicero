66e86003059c10379ff7da10a4af06aceda9dd49
Sebastien Rothlisberger <sebastien@rothlisberger.ca>
2017-12-23 17:57:24 -0500

    Add cli debugging

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,23 @@
 'use strict'
 
+const { spawn } = require('child_process')
+
 function run(args) {
   console.log('Hello!', args)
+
+  const ls = spawn('yarn', ['start'])
+
+  ls.stdout.on('data', data => {
+    console.log(`stdout: ${data}`)
+  })
+
+  ls.stderr.on('data', data => {
+    console.log(`stderr: ${data}`)
+  })
+
+  ls.on('close', code => {
+    console.log(`child process exited with code ${code}`)
+  })
 }
 
 module.exports = {
